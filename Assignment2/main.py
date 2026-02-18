bdb059f45a374df0904e5dd0e71ad9c26f82f7a5
Burman <bbedford@charlotte.edu>
2026-02-18 13:10:16 -0500

    Finished the main file, with the instances created.

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -14,6 +14,27 @@ cashier_instance = Cashier()
 
 def main():
     """write the rest of the codes"""
+    on = True
+    while on:
+        choice = input("What would you like? (small/ medium/ large/ off/ report): ").lower().strip()
+
+        if choice == "off":
+            on = False
+
+        elif choice == "report":
+            sandwich_maker_instance.report()
+
+        elif choice in recipes:
+            ingredients = recipes[choice]["ingredients"]
+            cost = recipes[choice]["cost"]
+
+            if sandwich_maker_instance.check_resources(ingredients):
+                coins = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(coins, cost):
+                    sandwich_maker_instance.make_sandwich(choice, ingredients)
+
+        else:
+            print("Invalid choice. Please try again.")
 
 if __name__=="__main__":
     main()
